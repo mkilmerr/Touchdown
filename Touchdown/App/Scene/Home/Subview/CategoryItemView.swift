6b03de4c3e51b6e8e8f7c33148ff296db9227aef
mkilmerr <marcoskilmer1@gmail.com>
2022-06-18 14:02:08 -0300

    CategoryItemView created 🏈

---
+++ Touchdown/App/Scene/Home/Subview/CategoryItemView.swift
@@ -0,0 +1,44 @@
+//
+//  CategoryGridItemView.swift
+//  Touchdown
+//
+//  Created by Marcos Kilmer Pereira de Aquino on 18/06/22.
+//
+
+import SwiftUI
+
+struct CategoryItemView: View {
+    let category: Category
+    var body: some View {
+        Button(action: {}, label: {
+            HStack(alignment: .center, spacing: 6.0) {
+                Image(category.image)
+                    .renderingMode(.template)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 30, height: 30, alignment: .center)
+                    .foregroundColor(.gray)
+
+                Text(category.name.uppercased())
+                    .fontWeight(.light)
+                    .foregroundColor(.gray)
+                Spacer()
+            }
+        })
+            .padding()
+            .background(Color.white.cornerRadius(12))
+            .cornerRadius(12)
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color.gray, lineWidth: 2.0)
+            )
+    }
+
+    struct CategoryGridItemView_Previews: PreviewProvider {
+        static var previews: some View {
+            CategoryItemView(category: Category(id: 1, name: "Jersey", image: "icon-jersey"))
+                .previewLayout(.sizeThatFits)
+                .background(Color("ColorBackground"))
+        }
+    }
+}
